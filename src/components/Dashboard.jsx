7910e66197a64a87ffd6de420f5ed4b1c6a8a4bd
Neathan Kungu <natekungu857@gmail.com>
2025-07-21 01:36:55 +0300

    Dashboard for viewing goal progress created with reference to prev components

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,1 +1,18 @@
+import GoalList from "./GoalList";
+import DepositSavings from "./DepositSavings";
+import GoalGuide from "./GoalGuide";
+import AddForm from "./AddForm";
 
+function Dashboard({ goals, setGoals }) {
+    return (
+        <div>
+            <h1>Goal Dashboard</h1>
+            <AddForm setGoals={setGoals} />
+            <GoalGuide goals={goals} />
+            <GoalList goals={goals} setGoals={setGoals} />
+            <DepositSavings goals={goals} setGoals={setGoals} />
+
+        </div>
+    )
+}
+export default Dashboard;
